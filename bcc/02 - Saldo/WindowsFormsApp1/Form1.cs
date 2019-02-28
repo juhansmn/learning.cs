b0e2c248b3728ce9e5ed0dba8805a0e958b71614
Juan Suman <victor.suman23@gmail.com>
2019-02-27 22:20:30 -0300

    Add files via upload
    Exercício do dia 27/02/2019 de Consulta de Saldo.

---
+++ bcc/02 - Saldo/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Humano homem;
+        Humano mulher;
+        public Form1()
+        {
+            InitializeComponent();
+            homem = new Humano();
+            mulher = new Humano();
+
+        }
+
+        private void btnDebitar_Click(object sender, EventArgs e)
+        {
+            double valor = Convert.ToDouble(txtValor.Text);
+            if (rbnHomem.Checked)
+                homem.Debitar(valor);
+            else
+                mulher.Debitar(valor);
+        }
+
+        private void btnCreditar_Click(object sender, EventArgs e)
+        {
+            double valor = Convert.ToDouble(txtValor.Text);
+            if (rbnHomem.Checked)
+                homem.Creditar(valor);
+            else
+                mulher.Creditar(valor);
+        }
+
+        private void btnVerSaldo_Click(object sender, EventArgs e)
+        {
+            if (rbnHomem.Checked)
+                lblSaldo.Text = homem.VerSaldo().ToString();
+            else
+                lblSaldo.Text = mulher.VerSaldo().ToString();
+        }
+
+        private void btnVerSaldoFamiliar_Click(object sender, EventArgs e)
+        {
+            lblSaldoFamiliar.Text = (homem.VerSaldo() + mulher.VerSaldo()).ToString();
+        }
+    }
+}
