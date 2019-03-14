7d87cafc8dee06d143000be125d41969d489b39b
Juan Suman <victor.suman23@gmail.com>
2019-03-13 22:33:31 -0300

    Exercício de Elfos e Anões da aula de POO.

---
+++ ElfosAnões/ElfosAnões/Personagem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElfosAnões
+{
+    class Personagem
+    {
+        Random aleatorio = new Random();
+
+        private String name;
+        public int vida { get; set; }
+        public int mana { get; set; }
+
+        public Personagem(String name)
+        {
+            this.name = name;
+            this.vida = aleatorio.Next(10, 100);
+            this.mana = aleatorio.Next(10, 100);
+        }
+
+        public string Lutar()
+        {
+            this.vida -= aleatorio.Next(30);
+            this.mana -= aleatorio.Next(20);
+
+            if (Morreu(vida))
+                return "Ele morreu!";
+            return "Ele está vivo!";
+        }
+
+        public bool Morreu(int vida)
+        {
+            if (vida == 0)
+                return true;
+            return false;
+        }
+
+    }
+}
